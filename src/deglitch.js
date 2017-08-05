fa02a1547c6dede5bbaaedef05ee1b740ae20c2b
fejesd <fejesd@gmail.com>
2017-08-06 01:15:33 +0200

    JsHint & Grunt added

--- src/deglitch.js
+++ src/deglitch.js
@@ -36,23 +36,23 @@ module.exports = function(RED) {
             if (!(topic in topics)) {                       // on first message, just pass immediately                
                 topics[topic] = {"value": payload, "message":msg, "timer":null};
                 node.send(msg);
-            } else if (topics[topic]['timer'] != null) {     // timer active                
-                if (payload == topics[topic]['value']) {         // the value has just returned to the original
+            } else if (topics[topic].timer != null) {     // timer active                
+                if (payload == topics[topic].value) {         // the value has just returned to the original
                     node.error('  ..shut down');
-                    clearTimeout(topics[topic]['timer']);           //so shut down the timer
-                    topics[topic]['timer'] = null;                     
+                    clearTimeout(topics[topic].timer);           //so shut down the timer
+                    topics[topic].timer = null;                     
                 } else {                                        // the new value is not the original..                    
-                    topics[topic]['newmessage'] = msg;
+                    topics[topic].newmessage = msg;
                 }
             } else {                                        // timer is inactive, this is a new event                
-                topics[topic]['newmessage'] = msg;
-                topics[topic]['timer'] = setTimeout(function(){
+                topics[topic].newmessage = msg;
+                topics[topic].timer = setTimeout(function(){
                     node.error('timeout');
-                    node.send(topics[topic]['newmessage']);
-                    topics[topic]['message'] = topics[topic]['newmessage'];
-                    topics[topic]['value'] = JSON.stringify(topics[topic]['message'].payload);
-                    topics[topic]['newmessage'] = undefined;
-                    topics[topic]['timer'] = null;
+                    node.send(topics[topic].newmessage);
+                    topics[topic].message = topics[topic].newmessage;
+                    topics[topic].value = JSON.stringify(topics[topic].message.payload);
+                    topics[topic].newmessage = undefined;
+                    topics[topic].timer = null;
                 }, timeout_sec*1000);
             }
         });        
